807536c724569806480f72ebe74293cd41064ab8
Dysax <lukedysax@gmail.com>
2024-10-01 17:16:08 -0500

    some changes

--- config/appearance.lua
+++ config/appearance.lua
@@ -39,9 +39,15 @@ return {
    show_tab_index_in_tab_bar = false,
    switch_to_last_active_tab_when_closing_tab = true,
 
+   status_update_interval = 1000,
+
+   tab_bar_at_bottom = false,
+
+   
+
    -- window
    window_padding = {
-      left = 0,
+      left = 5,
       right = 0,
       top = 10,
       bottom = 7.5,
--- config/fonts.lua
+++ config/fonts.lua
@@ -2,7 +2,9 @@ local wezterm = require('wezterm')
 local platform = require('utils.platform')
 
 local font = 'JetBrainsMono Nerd Font'
-local font_size = platform().is_mac and 12 or 9
+--local font = 'MesloLGL Nerd Font'
+--local font = 'FiraCode Nerd Font'
+local font_size = platform().is_mac and 12 or 14
 
 return {
    font = wezterm.font(font),
